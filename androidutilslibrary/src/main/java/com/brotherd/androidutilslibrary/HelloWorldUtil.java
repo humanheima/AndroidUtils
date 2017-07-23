8a76c087e7343e9b58e4e00e502fa792f5b72d6e
humanheima <humanheima@gmail.com>
2017-07-23 15:03:01 +0800

    测试上传到bintray

---
+++ androidutilslibrary/src/main/java/com/brotherd/androidutilslibrary/HelloWorldUtil.java
@@ -0,0 +1,18 @@
+package com.brotherd.androidutilslibrary;
+
+import android.util.Log;
+
+/**
+ * Created by dumingwei on 2017/7/23.
+ */
+public class HelloWorldUtil {
+
+    private static final String TAG = "HelloWorldUtil";
+
+    public HelloWorldUtil() {
+    }
+
+    public static void seyHelloWorld() {
+        Log.e(TAG, "hello world");
+    }
+}
